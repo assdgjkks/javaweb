e734dd573fa8b9516d435db2383653a7a1ccd3ad
zhou <zhou@jian.com>
2023-01-28 21:22:30 +0800

    hot fix

--- servlet01/src/com/bjpower/javaweb/servlet/Text.java
+++ servlet01/src/com/bjpower/javaweb/servlet/Text.java
@@ -2,8 +2,10 @@ package com.bjpower.javaweb.servlet;
 
 public class Text {
     public static void main(String[] args) {
-        System.out.println("啦啦啦啦啦");
-        System.out.println("天天");
-        System.out.println("啦啦啦");
+        System.out.println("hot fix");
+        System.out.println("hot fix");
+        System.out.println("hot fix");
+        System.out.println("hot fix");
+
     }
 }
