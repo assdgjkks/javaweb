9b809960a23d3376d0cb24af20d6c38d7bedbc1f
zhou <zhou@jian.com>
2023-01-28 13:44:39 +0800

    second commit

--- servlet01/src/com/bjpower/javaweb/servlet/Text.java
+++ servlet01/src/com/bjpower/javaweb/servlet/Text.java
@@ -3,5 +3,6 @@ package com.bjpower.javaweb.servlet;
 public class Text {
     public static void main(String[] args) {
         System.out.println("啦啦啦啦啦");
+        System.out.println("天天");
     }
 }
